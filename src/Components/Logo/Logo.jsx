4509d60f54a68fb24ef5dd879877ad19c8662732
Hasib Ahamed Sakib <hasibahamedsakib1@gmail.com>
2024-05-08 00:14:21 +0600

    fixing some responsive ishu

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Logo = () => {
   return (
-    <div className="text-center ">
+    <a href="#" className="text-center ">
       <h1 className="text-xl font-bold leading-none font-mono text-slate-700">
         Hasib
       </h1>
       <h1 className="text-lg  leading-none font-sans ">Ahamed Sakib</h1>
-    </div>
+    </a>
   );
 };
 
